c3dd78fd70a5999538e4c48d834be32d71781441
Stan <dimitrov.s.dev@gmail.com>
2023-03-01 17:56:34 +0200

    Regular_Expressions

--- Whitespace_Characters_and_Boundaries/Exercise.py
+++ Whitespace_Characters_and_Boundaries/Exercise.py
@@ -4,16 +4,16 @@
 # characters at the end, preparing to replace them
 # with an empty string. Contents of the string before
 # the spaces is not important
-trailing_space_regex = r''
+trailing_space_regex = r' +$'
 
 # Exercise 10: Match strings that contain two tabs in a row
 # anywhere in the string. Contents of the string are unimportant.
-two_consecutive_tabs_regex = r''
+two_consecutive_tabs_regex = r'\t\t'
 
 # Exercise 11: Match strings that contain two tabs, not
 # necessarily in a row, anywhere in the string.
 # Contents of the string are unimportant.
-two_tabs_regex = r''
+two_tabs_regex = r'\t.*\t'
 
 # Exercise 12: Match strings that start with at least
 # three digits from 0 to 5 (inclusive).
