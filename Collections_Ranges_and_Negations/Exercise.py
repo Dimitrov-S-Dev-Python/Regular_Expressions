701f0f0c6848afcfad372f917b8a2db4f48de6e1
Stan <dimitrov.s.dev@gmail.com>
2023-03-01 11:11:13 +0200

    Regular_Expressions

--- Collections_Ranges_and_Negations/Exercise.py
+++ Collections_Ranges_and_Negations/Exercise.py
@@ -2,7 +2,7 @@
 
 # Exercise 5: Match a string that represents a binary number
 # (contains only 0s and 1s)
-binary_regex = r''
+binary_regex = r'[0-1]+'
 
 # Exercise 6: Match a potential Hawaiian word
 # from this collection of letters: AEIOUHKLMNPW
@@ -10,12 +10,12 @@ binary_regex = r''
 # First letter can be lowercase or capital; other
 # letters must be lowercase. The word must contain one
 # or more letters.
-hawaiian_regex = r''
+hawaiian_regex = r'[AEIOUHKLMNPWaeiouhklmnpw][aeiouhklmnpw\']*'
 
 # Exercise 7: Match a telephone number of the format 555-555-5555
 # (where each of the 5s could be any number)
-telephone_regex = r''
+telephone_regex = r'[0-9]{3}-[0-9]{3}-[0-9]{4}'
 
 # Exercise 8: Match strings that are at least six characters
 # and don’t contain the letter A (uppercase or lowercase)
-six_plus_no_a_regex = r''
+six_plus_no_a_regex = r'[^aA]{6,}'
